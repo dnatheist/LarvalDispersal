4d348cae9065b83caa39fad876b2d1b2f91ed6d7
Alan Couch <alan@couch.net.au>
2015-04-06 20:50:03 +1000

    First version of working LDA

---
+++ src/lda.R
@@ -0,0 +1,12 @@
+## @knitr lda
+
+# Linear Discriminant Analysis with Jacknifed Prediction 
+library(MASS)
+fit <- lda(SiteName ~ Delta13C + CNRatio+B+K+V+Rb, data=ImportantVars, na.action="na.omit", CV=TRUE)
+fit # show results
+# Assess the accuracy of the prediction
+# percent correct for each category of G
+ct <- table(ImportantVars$SiteName, fit$class)
+diag(prop.table(ct, 1))
+# total percent correct
+sum(diag(prop.table(ct)))
