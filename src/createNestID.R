4cbff6e4e70df6d69465c13c51a491da55c51152
dnatheist <alan@couch.net.au>
2015-08-06 21:08:47 +1000

    Calculate nest label. Included lines 26-58 from genetics chapter

--- src/createNestID.R
+++ src/createNestID.R
@@ -1,5 +1,7 @@
 #This file is to generate names for nests. By making rules to do it based only on location and day of hatch I can then compare to siblings list. This should give and idea of error maybe.
 
+# I suppose this is actually a classification problem. Perhaps should treat as such, do confusion matrix etc.
+
 
 #Establish Nest Distance based on Dispersal Velocity
 dispersalVelocity<-1200
@@ -17,11 +19,41 @@ plot(larv2$nestDist,larv2$clade, xlab="Nest Distance (m)", ylab = "Clade")
 larv2$roundedHatchDoY<-round(larv2$hatchDoY)
 larv2$nestSeg<-round(larv2$nestDist/100)
 larv2$siteLetter<-substr(larv2$SiteName, 1, 1)
-ndDF<-subset(larv2, select=c(LarvalID,nestSeg,roundedHatchDoY,siteLetter))
+ndDF<-subset(larv2, select=c(Label,nestSeg,roundedHatchDoY,siteLetter))
 
 #create the name (still no sibs accounted for)
 ndDF$nestIDnoSibs<-paste(ndDF$nestSeg,ndDF$siteLetter,ndDF$roundedHatchDoY, sep="")
 
+
+#Calculate a family group
+families<-as.data.frame(cutree(dend2, h = 26.5))
+larvalClades<-merge(larvalClades,families, by="row.names")
+names(larvalClades)[names(larvalClades)=="cutree(dend2, h = 26.5)"] <- "Family"
+names(larvalClades)[names(larvalClades)=="Row.names"] <- "Label"
+
+
+#calc nest distance
+larv$nestDist<-((larv$Distance.to.Angle.Crossing..m.-(dispersalVelocity*(larv$Day.of.Year-((larv$Day.of.Year-(larv$ageOL)))+larv$incubTime))))
+
+new<-merge(larv,larvalClades, by="Label")
+newsub<-new
+
+
+#Create a data frame from which to develop nestID names (not yet combining siblings)
+new$roundedHatchDoY<-round(new$hatchDoY)
+new$nestSeg<-round(new$nestDist/100)
+new$siteLetter<-substr(new$Label, 1, 1)
+
+famNo<-unique(new$Family)
+final<- newsub[FALSE,]
+
+for (nn in famNo){
+        newsub<-subset(new,Family==nn, select=c(LarvalID,Label,nestSeg,YearOnly,siteLetter,roundedHatchDoY,Family))
+        newsub$nestID<-paste(round(mean(newsub$nestSeg)),newsub$siteLetter,round(mean(newsub$roundedHatchDoY)), sep="")
+        final<-rbind(final,newsub)
+}
+plot(final$nestSeg,as.factor(final$nestID))
+nrow(as.data.frame(unique(final$nestID)))
 #Output a file
 #write.csv(ndDF, file="ndDF.csv")
 
@@ -36,8 +68,8 @@ doy<-as.matrix(dist(subset(ndDF, select=c(roundedHatchDoY))))
 seg[lower.tri(seg, diag=TRUE)] <- NA
 
 #Create logical matrices
-segLogical<-seg<5 #Less than 500m apart
-doyLogical<-doy<3 #Hatch day less than days apart
+segLogical<-seg<7 #Less than 500m apart
+doyLogical<-doy<5 #Hatch day less than days apart
 
 #Combine them
 sameNestEst<-segLogical*doyLogical
@@ -72,8 +104,56 @@ c<-as.data.frame(colnames(sameNestEstSubset)[k[,2]])
 sibEstPairs<-as.data.frame(c(r,c))
 
 #tidy up var names
-names(sibEstPairs)[names(sibEstPairs) == "rownames.sameNestEstSubset..k...1.."] <- 'row'
-names(sibEstPairs)[names(sibEstPairs) == "colnames.sameNestEstSubset..k...2.."] <- 'col'
+names(sibEstPairs)[names(sibEstPairs) == "rownames.sameNestEstSubset..k...1.."] <- 'rown'
+names(sibEstPairs)[names(sibEstPairs) == "colnames.sameNestEstSubset..k...2.."] <- 'coln'
 
-#sort
-sibEstPairs<-sibEstPairs[order(row),] #not yet working
+#Order by row (effectively LarvalID) so each Larval ID has all siblings listed.
+sibEstPairs<-sibEstPairs[order(sibEstPairs$rown),]
+
+#Create an actual sibs list so we can compare the two
+###########
+# Recreate Genetic Distance
+# Create a Murray Cod distance matrix
+MCdm<-MCsnps[-c(1:7),]
+MCdm <- dist(MCdm)
+MCdm<-as.matrix(MCdm)
+MCdm<-as.data.frame(MCdm)
+#This is to be used for plotting
+MCdmForSibs<-MCdm #need this matrix for sibling analysis .r
+
+#Then create actual sibs list
+sibsSubset<-subset(MCdmForSibs, apply(MCdmForSibs, 1, function(MCdmForSibs){any(MCdmForSibs > 0 & MCdmForSibs < 26.5)}))
+#The siblings have a dissimilarity index of less than 26.5. This can be seen in the dendrogram and in the scatterplot for IBD.
+
+sibsLogical<-sibsSubset<26.5 #a logical DF to record T and F for sibling pairs
+#sibsSubset<-sibsLogical*sibsSubset #This turns to 0 all the genetic similarity above 26.5 (no-sibs)
+
+#sibsSubset<-sibsSubset[!sapply(sibsSubset, function(x) all(x == 0))] #now remove those with no sibs
+#
+#sibSubset<-sibSubset<0
+
+sibsLogical[lower.tri(sibsLogical, diag=TRUE)] <- NA
+
+#########
+ai<-which(sibsLogical ==TRUE)
+k <- arrayInd(ai, dim(sibsLogical))
+
+#sibEstPairs<-data.frame(x=1,y=2)
+r<-as.data.frame(rownames(sibsLogical)[k[,1]])
+c<-as.data.frame(colnames(sibsLogical)[k[,2]])
+rm(sibsLogical)
+sibActualPairs<-as.data.frame(c(r,c))
+
+#tidy up var names
+names(sibActualPairs)[names(sibActualPairs) == "rownames.sibsLogical..k...1.."] <- 'rown'
+names(sibActualPairs)[names(sibActualPairs) == "colnames.sibsLogical..k...2.."] <- 'coln'
+
+#Order by row (effectively LarvalID) so each Larval ID has all siblings listed.
+sibActualPairs<-sibActualPairs[order(sibActualPairs$rown),]
+###########
+
+intersect(sibActualPairs$coln, sibEstPairs$coln)
+
+########################
+
+
