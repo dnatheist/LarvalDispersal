d2b768c5b604e391892f9cbec31de7fef80e9cd6
dnatheist <alan@couch.net.au>
2015-10-22 08:23:13 +1100

    New file

---
+++ confusionMatrixExamples.R
@@ -0,0 +1,25 @@
+## 3 class example
+
+confusionMatrix(iris$Species, sample(iris$Species))
+
+newPrior <- c(.05, .8, .15)
+names(newPrior) <- levels(iris$Species)
+
+confusionMatrix(iris$Species, sample(iris$Species))
+
+#2 class
+
+lvs <- c("normal", "abnormal")
+truth <- factor(rep(lvs, times = c(86, 258)),
+                levels = rev(lvs))
+pred <- factor(
+        c(
+                rep(lvs, times = c(54, 32)),
+                rep(lvs, times = c(27, 231))),               
+        levels = rev(lvs))
+
+xtab <- table(pred, truth)
+
+confusionMatrix(xtab)
+confusionMatrix(pred, truth)
+confusionMatrix(xtab, prevalence = 0.25) 
